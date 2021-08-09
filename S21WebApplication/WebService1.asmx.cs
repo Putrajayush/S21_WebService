1dd038d32b6461edd12a3f3ef3a2572fea055d99
putra <putrajayush11@Gmail.com>
2021-08-09 08:20:14 +0700

    Menambahkan thread pada WebApplication, terdapat beberapa bug

--- S21WebApplication/WebService1.asmx.cs
+++ S21WebApplication/WebService1.asmx.cs
@@ -63,5 +63,65 @@ namespace S21WebApplication
             string result = JsonConvert.SerializeObject(dtUsers);
             return result;
         }
+
+        public void Main(string[] args)
+        {
+            Queue<Order> ordersQueue = new Queue<Order>();
+
+            foreach (Order o in ReceiveOrdersFromBranch1())
+            {
+                ordersQueue.Enqueue(o);
+            }
+
+            foreach (Order o in ReceiveOrdersFromBranch2())
+            {
+                ordersQueue.Enqueue(o);
+            }
+
+            while (ordersQueue.Count > 0)
+            {
+
+                Order currentOrder = ordersQueue.Dequeue();
+                currentOrder.ProcessOrder();
+            }
+        }
+
+        static Order[] ReceiveOrdersFromBranch1()
+        {
+            Order[] orders = new Order[]
+            {
+                new Order(1,5),
+                new Order(2,4),
+                new Order(6,10)
+            };
+            return orders;
+        }
+
+        static Order[] ReceiveOrdersFromBranch2()
+        {
+            Order[] orders = new Order[]
+            {
+                new Order(3,5),
+                new Order(4,4),
+                new Order(5,10)
+            };
+            return orders;
+        }
+
+        class Order
+        {
+            public int OrderId { get; set; }
+            public int OrderQuantity { get; set; }
+            public Order(int id, int orderQuantity)
+            {
+                this.OrderId = id;
+                this.OrderQuantity = orderQuantity;
+            }
+
+            public void ProcessOrder()
+            {
+                Console.WriteLine($"Order {OrderId} processed!.");
+            }
+        }
     }
 }
